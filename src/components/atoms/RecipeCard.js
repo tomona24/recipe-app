b0ce021593cdbe0037828da308bf3d0f5f764b8e
Tomona Sako <sakotomona@gmail.com>
2020-12-18 11:49:06 +0900

    fix

--- src/components/atoms/RecipeCard.js
+++ src/components/atoms/RecipeCard.js
@@ -47,9 +47,6 @@ const useStyles = makeStyles((theme) => ({
   expandOpen: {
     transform: 'rotate(180deg)',
   },
-  avatar: {
-    backgroundColor: red[500],
-  },
   formControl: {
     minWidth: 60,
   },
